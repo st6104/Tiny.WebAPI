0dcc370ef565b049799cfc230bc3815323bec871
어준일 <st6104@iquest.co.kr>
2023-04-03 16:24:34 +0900

    ForeachAsync 확장메소드 개선

--- Tiny.Shared/Extensions/IEnumerableExtension.cs
+++ Tiny.Shared/Extensions/IEnumerableExtension.cs
@@ -2,14 +2,14 @@ namespace Tiny.Shared.Extensions;
 
 public static class IEnumerableExtensions
 {
-    public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> func, CancellationToken cancellationToken)
+    public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, CancellationToken, Task> func, CancellationToken cancellationToken = default)
     {
         foreach (var item in source)
         {
             if(cancellationToken.IsCancellationRequested)
                 return;
 
-            await func(item);
+            await func(item, cancellationToken);
         }
     }
 }
